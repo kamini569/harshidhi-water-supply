1ba0ca29b8ca1e0af0033bd21956621a6d33d864
Kamini Parmar <kaminiparmar31489@gmail.com>
2026-02-27 23:55:45 +0530

    First commit

---
+++ harshidhi_water_supply/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for harshidhi_water_supply project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'harshidhi_water_supply.settings')
+
+application = get_asgi_application()
