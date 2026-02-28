b3294c63405d77a61a85a5418d30d0a374b4c815
Kamini Parmar <kaminiparmar31489@gmail.com>
2026-02-28 22:56:30 +0530

    seventh commit

--- water/urls.py
+++ water/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('services/', views.services, name='services'),
-    path('pricing/', views.pricing, name='pricing'),
-    path('order/', views.order, name='order'),
-    path('contact/', views.contact, name='contact'),
+path('services/', views.services, name='services'),
+path('pricing/', views.pricing, name='pricing'),
+path('order/', views.order, name='order'),
+path('contact/', views.contact, name='contact'),
 ]
